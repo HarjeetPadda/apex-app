f3a2593e7916134d7d8195538d24f3bad546d2ca
HarjeetPadda <harjeetpadda@gmail.com>
2022-08-22 17:44:41 +1000

    faker test

--- dbtest.php
+++ dbtest.php
@@ -1,19 +1,36 @@
+<!--<!--consign-->-->
+<!--//include ('database.php');-->
+<!--////-->
+<!--////$obj=new query();-->
+<!--////$selectCondition_Arr=array();-->
+<!--////$insertCondition_Arr=array('Sender_Address'=>'test1 sender', 'Receiver_Address'=>'test1 receiver', 'Customer'=>'test1 customer', 'Equipment_Freight_Type'=>'test1 freight', 'Trailer_1'=>'test1 trailer1', 'Trailer_2'=>'test1 trailer2');-->
+<!--////$deleteCondition_Arr=array('Id'=>'17');-->
+<!--////$updateCondition_Arr=array('Sender_Address'=>'test2', 'Receiver_Address'=>'test2', 'Customer'=>'test2', 'Equipment_Freight_Type'=>'test2', 'Trailer_1'=>'test2', 'Trailer_2'=>'test2');-->
+<!--////-->
+<!--//////$result=$obj->getData('*','consignments',$selectCondition_Arr,'Id','DESC','',);-->
+<!--////-->
+<!--////-->
+<!--////$result=$obj->insertData('consignments',$insertCondition_Arr);-->
+<!--////-->
+<!--////-->
+<!--//////$result=$obj->deleteData('consignments',$deleteCondition_Arr);-->
+<!--////-->
+<!--////-->
+<!--//////$result=$obj->updateData('consignments',$updateCondition_Arr,'Id','17');-->
+
 <?php
-include ('database.php');
 
-$obj=new query();
-$selectCondition_Arr=array();
-$insertCondition_Arr=array('Sender_Address'=>'test1 sender', 'Receiver_Address'=>'test1 receiver', 'Customer'=>'test1 customer', 'Equipment_Freight_Type'=>'test1 freight', 'Trailer_1'=>'test1 trailer1', 'Trailer_2'=>'test1 trailer2');
-$deleteCondition_Arr=array('Id'=>'17');
-$updateCondition_Arr=array('Sender_Address'=>'test2', 'Receiver_Address'=>'test2', 'Customer'=>'test2', 'Equipment_Freight_Type'=>'test2', 'Trailer_1'=>'test2', 'Trailer_2'=>'test2');
+require_once 'vendor/autoload.php';
 
-//$result=$obj->getData('*','consignments',$selectCondition_Arr,'Id','DESC','',);
+$faker = Faker\Factory::create();
 
+$conn=mysqli_connect('localhost','root','','apex');
+if(!$conn){
+    echo('Failed').mysqli_connect_error();
+}else{
+    echo ('Connected');
+}
 
-//$result=$obj->insertData('consignments',$insertCondition_Arr);
+$sql = new mysqli($conn);
 
-
-//$result=$obj->deleteData('consignments',$deleteCondition_Arr);
-
-
-//$result=$obj->updateData('consignments',$updateCondition_Arr,'Id','17');
+$sql->query("INSERT INTO `consignments` (`Id`, `Date`, `Sender_Address`, `Receiver_Address`, `Customer`, `Equipment_Freight_Type`, `Trailer_1`, `Trailer_2`) VALUES (NULL, $faker->Date(), 'dsd', 'sds', 'sds', 'sds', 'sd', 'sd')");
